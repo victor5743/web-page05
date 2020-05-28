5d7725f210bbc9f09b655de8baa27a08f7c5cbfb
Victor <vcs5743@gmail.com>
2020-05-28 11:47:16 -0400

    Header component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-	<div class="font-roboto font-bold" id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+	<div class="font-roboto text-gray-800" id="app">
+		<Header />
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
 
 export default {
 	name: 'App',
 	components: {
-		HelloWorld,
+		Header,
 	},
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt illo, architecto
+		eligendi ratione fugit dolore voluptate in pariatur magni quod numquam similique quo
+		perferendis atque exercitationem expedita sit. Alias, sed.
+	</p>
+</template>
+
+<script>
+export default {};
+</script>
