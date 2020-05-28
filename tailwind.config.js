712446a8443fa0303d3f559523ceb7c97ed28261
Victor <vcs5743@gmail.com>
2020-05-28 11:10:08 -0400

    Roboto font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+	purge: [],
+	theme: {
+		extend: {
+			fontFamily: {
+				roboto: ['roboto'],
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
 };
